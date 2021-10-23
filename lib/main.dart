fc66b2dfa6dd5dc00aa0c414a5cfcd024107ec18
Luis Jimenez <luisrrjjcc@gmail.com>
2021-10-22 18:50:34 -0600

    Alarmas funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'package:early_bird/pages/real_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+
 void main () => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -70,6 +73,9 @@ class AlarmScreen extends StatelessWidget {
 
   void _selectTime(context) async {
     final AlarmBloc alarmBloc = Provider.of<AlarmBloc>(context, listen: false);
+    final ToDoBloc toDoBloc = Provider.of<ToDoBloc>(context, listen: false);
+
+    AndroidAlarmManager.initialize();
 
     final TimeOfDay? newTime = await showTimePicker(
       context: context,
@@ -77,14 +83,60 @@ class AlarmScreen extends StatelessWidget {
     );
     if (newTime != null) {
       alarmBloc.timeOfDay = newTime;
+
+      /*
+      await AndroidAlarmManager.oneShot(
+          const Duration(seconds: 5),
+          123,
+          alarmCallback,
+          alarmClock: true,
+          allowWhileIdle: true,
+          wakeup: true
+      );
+      */
+
+      int tasks = toDoBloc.countPendingTasks();
+      TimeOfDay real = alarmBloc.substractMinutesPerTask(15, tasks);
+
+      DateTime now = DateTime.now();
+      DateTime alarmTime = DateTime(now.year, now.month, now.day, real.hour, real.minute);
+
+      if ( (real.hour < now.hour) || (real.hour == now.hour && real.minute < now.minute) ) {
+        // Si la hora de la alarma es menor
+        // O si empatan las horas, comparar minutos
+        // --> La alarma es mañana
+
+        alarmTime = alarmTime.add(const Duration(days: 1));
+      }
+
+      await AndroidAlarmManager.oneShotAt(
+        alarmTime,
+        123,
+        alarmCallback,
+        alarmClock: true,
+        allowWhileIdle: true,
+        wakeup: true
+      );
+
+      print('Alarma colocada');
+      print(alarmTime.toString());
     }
+  }
+
+  static Future<void> alarmCallback() async {
+    print('ALARMA DISPARADA');
+
+    /*
+      Se debe cerrar la aplicación para silenciar el audio
+      Si la aplicación está cerrada, debe abrirse y luego cerrarse
+    */
+
+    FlutterRingtonePlayer.playAlarm();
   }
 
   Widget build(BuildContext context) {
     final ToDoBloc toDoBloc = Provider.of<ToDoBloc>(context, listen: false);
     final AlarmBloc alarmBloc = Provider.of<AlarmBloc>(context, listen: false);
-
-
 
     return Scaffold(
       appBar: AppBar(
@@ -99,20 +151,12 @@ class AlarmScreen extends StatelessWidget {
               },
               child: Text('Elegir hora'),
             ),
-            /*
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Regresar'),
-            ),
-            */
             Text('\n\nHora planeada para alarma:'),
             SizedBox(
               height: 30,
               child: AlarmPage(),
             ),
-            Text('\n\nHora real:'),
+            Text('\n\nHora real en que sonará:'),
             SizedBox(
               height: 30,
               child: RealPage(),
